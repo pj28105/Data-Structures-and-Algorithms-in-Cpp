f617e2de0789c6af7cc24fca08d583649cd8406e
pj28105v1 <piyushjain964349@gmail.com>
2020-05-01 12:00:23 +0530

    Added vertical order traversal recursive and iterative

---
+++ Trees/Traversals/Vertical_Order_traversal_recursive.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Prefer these questions to do iteratively with level order traversal only!
+    This question also teaches that use always stable_sort instead of sort in c++
+    as sort may not be stable all the time but stable sort ensures stability in sorting.
+    If you are doing this question recursively sort the map of vector with stable_sort in decrasing order of lvl 
+*/
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+void verticalOrder(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+          
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+// Function for Inorder Traversal
+void printInorder(Node* root)
+{
+    if(!root)
+        return;
+        
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        // string c;
+        // getline(cin,c);
+    	Node* root = buildTree(s);
+    	
+    	verticalOrder(root);
+        cout << endl;
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
+
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child 
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+// root: root node of the tree
+void solv(Node* root,map<int,vector<pair<int,int>>>& ma,int hd,int lvl){
+    if(!root){
+        return;
+    }
+    ma[hd].push_back({root->data,lvl});
+    solv(root->left,ma,hd - 1,lvl+1);
+    solv(root->right,ma,hd + 1,lvl+1);
+    return;
+}
+void verticalOrder(Node *root){
+    if(!root){
+        return;
+    }
+    map<int,vector<pair<int,int>>> ma;
+    solv(root,ma,0,0);
+
+    // Note to print the vector in same loop use &i
+    for(auto i: ma){
+        stable_sort(ma[i.first].begin(),ma[i.first].end(),[](pair<int,int>a,pair<int,int>b)->bool{
+            return a.second < b.second; 
+        });
+    }
+    for(auto i : ma){
+        for(auto j : i.second){
+            cout << j.first << " ";
+        }
+    }
+}
---
+++ Trees/Traversals/Vertical_order_traversal.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Vertical Order Traversal of Binary Tree
+    Code tested on GFG
+    for recursive solution check github but prefer iterative approach for these kind of
+    questions!
+*/
+
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+void verticalOrder(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+          
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+// Function for Inorder Traversal
+void printInorder(Node* root)
+{
+    if(!root)
+        return;
+        
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        // string c;
+        // getline(cin,c);
+    	Node* root = buildTree(s);
+    	
+    	verticalOrder(root);
+        cout << endl;
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
+
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child 
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+// root: root node of the tree
+void verticalOrder(Node *root){
+    if(!root){
+        return;
+    }
+    int min_hd = 0,max_hd = 0;
+    unordered_map<int,vector<int>>ma;
+    queue<pair<Node*,int>>q;
+    q.push({root,0});
+    while(!q.empty()){
+        pair<Node*,int> curr = q.front();
+        q.pop();
+        min_hd = min(curr.second,min_hd);
+        max_hd = max(curr.second,max_hd);
+        ma[curr.second].push_back(curr.first->data);
+        if(curr.first->left){
+            q.push({curr.first->left,curr.second - 1});
+        }
+        if(curr.first->right){
+            q.push({curr.first->right,curr.second + 1});
+        }
+    }
+    for(int i = min_hd; i <= max_hd; i++){
+        for(auto j : ma[i]){
+            cout << j << " ";
+        }
+    }
+}
