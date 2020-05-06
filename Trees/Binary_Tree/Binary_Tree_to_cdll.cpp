c8b31a802dbcbff695a1d6cf4dd588a5bf7933f6
pj28105v1 <piyushjain964349@gmail.com>
2020-05-06 11:55:22 +0530

    Added binary tree to cdll and dll

---
+++ Trees/Binary_Tree/Binary_Tree_to_cdll.cpp
@@ -0,0 +1,173 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Problem link->
+    https://practice.geeksforgeeks.org/problems/binary-tree-to-cdll/1
+*/
+
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+Node *bTreeToCList(Node *root);
+void displayCList(Node *head)
+{
+    Node *itr = head;
+    do
+    {
+        cout << itr->data <<" ";
+        itr = itr->right;
+    } while (head!=itr);
+    cout <<endl;
+    itr=itr->left;
+    do{
+        cout<<itr->data<<" ";
+        itr=itr->left;
+    }while(head!=itr);
+    cout<<itr->data<<endl;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        Node *head = bTreeToCList(root);
+        displayCList(head);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+void solv(Node* root,Node* &head,Node* &curr){
+    if(!root){
+        return;
+    }
+    solv(root->left,head,curr);
+    if(!curr){
+        head = root;
+        curr = root;
+    }else{
+        curr->right = root;
+        root->left = curr;
+        curr = root;
+    }
+    solv(root->right,head,curr);
+}
+Node *bTreeToCList(Node *root){
+    Node* head = NULL,*curr = NULL;
+    solv(root,head,curr);
+    head->left = curr;
+    curr->right = head;
+    return head;
+}
