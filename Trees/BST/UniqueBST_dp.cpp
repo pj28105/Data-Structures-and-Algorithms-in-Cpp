bf0811725cafb9b3323ee223bb9fc57228283b64
pj28105v1 <piyushjain964349@gmail.com>
2020-05-08 17:26:35 +0530

    Added unique bst dp solutions

---
+++ Trees/BST/UniqueBST_dp.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+    Problem link->
+        https://practice.geeksforgeeks.org/problems/unique-bsts-1587115621/1/?track=amazon-bst&batchId=192
+*/
+
+const int mod = 1e9 + 7;
+
+unordered_map<int,int>dp;
+
+int numTrees(int n) {
+    if(n == 1 || n == 0)
+        return 1;
+        
+    if(n == 2){
+        return 2;
+    }
+    
+    if(dp.find(n) != dp.end())
+        return dp[n];
+    
+    int ans = 0;
+    
+    for(int i = 1; i <= n; i++){
+        ans = (ans + (((long long)numTrees(i - 1) * numTrees(n - i)  ) % mod))%mod; 
+    }
+    dp[n] = ans;
+    return ans;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<numTrees(n)<<"\n";
+    }
+}
