48ed9e1d67c6caed4c9420f08148d80d79cfbe6a
pj28105 <piyushjain964349@gmail.com>
2020-05-10 22:04:09 +0530

    Added mother vertex.cpp and find cycle in undirexted graph

---
+++ Graphs/Cycle_Detection/Undirected_Graph/check_cycle_through_dfs.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+    Problem link->
+    https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+
+    Most simplest way to find cycle in an undirected graph.
+    This solution also help us to detect self loop in graph.
+*/
+
+bool dfs(int s,bool vis[],vector<int>g[],int parent){
+    vis[s] = 1;
+    bool hasCycle = 0;
+    for(int v : g[s]){
+        if(vis[v] && v != parent){
+            return 1;
+        }
+        if(!vis[v])
+            hasCycle = hasCycle || dfs(v,vis,g,s);
+    }
+    return hasCycle;
+}
+
+bool isCyclic(vector<int> g[], int n){
+    bool vis[n];
+    bool hasCycle = 0;
+    memset(vis,0,sizeof(vis));
+    for(int i = 0; i < n; i++){
+        if(!vis[i]){
+            hasCycle = hasCycle || dfs(i,vis,g,-1);
+        }
+    }
+    return hasCycle;
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int V, E;
+        cin>>V>>E;
+        
+        // array of vectors to represent graph
+        vector<int> adj[V];
+        
+        int u, v;
+        for(int i=0;i<E;i++)
+        {
+            cin>>u>>v;
+            
+            // adding edge to the graph
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        cout << isCyclic(adj, V)<<endl;
+
+    }
+}
+ 
---
+++ Graphs/Mother_Vertex.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+/*
+    Problem link->
+    https://practice.geeksforgeeks.org/problems/mother-vertex/1
+
+    Perform DFS traversal of the given graph. While doing traversal keep track of last finished vertex ‘v’.
+    If there exist mother vertex, then v must be one. Check if v is a mother vertex by performing DFS/BFS from v.
+*/
+
+
+void dfs(int s,bool vis[],vector<int>g[]){
+    vis[s] = 1;
+    for(int v : g[s]){
+        if(!vis[v])
+            dfs(v,vis,g);
+    }
+    return;
+}
+
+int findMother(int n, vector<int> g[]) { 
+    int last;
+    bool vis[n];
+    memset(vis,0,sizeof(vis));
+    for(int i = 0; i < n; i++){
+        if(!vis[i]){
+            last = i;
+            dfs(i,vis,g);
+        }
+    }
+    memset(vis,0,sizeof(vis));
+    dfs(last,vis,g);
+    for(int i = 0; i < n; i++){
+        if(!vis[i])
+            return -1;
+    }
+    return last;
+} 
+
+int main() 
+{ 
+    int T;
+    cin>>T;
+    while(T--){
+        int num, edges;
+        cin>>num>>edges;
+        
+        vector<int> adj[num];
+        
+        int u, v;
+        while(edges--){
+            cin>>u>>v;
+            adj[u].push_back(v);
+        }
+        
+        cout<<findMother(num, adj)<<endl;
+    }
+
+	return 0;
+}
