cb40e0c053a8b6e9118cd3738d10a29a950f9d42
pj28105 <piyushjain964349@gmail.com>
2020-05-12 20:55:21 +0530

    Added word break 2

---
+++ Dp/Word_break_2.cpp
@@ -0,0 +1,47 @@
+/*
+    Problem link->
+    https://leetcode.com/problems/word-break-ii
+
+    Backtracking optimize by dp.
+*/
+
+
+unordered_map<int,vector<string>>dp;
+class Solution {
+public:
+    vector<string> solv(string s,unordered_set<string>&se,int in){
+        if(in >= s.length()){
+            return {""};
+        }
+        if(dp.find(in) != dp.end()){
+            return dp[in];
+        }
+        string aux = "";
+        vector<string> ans;
+        for(int i = in; i < s.length(); i++){
+            aux += s[i];
+            if(se.find(aux) != se.end()){
+                vector<string>temp = solv(s,se,i + 1);
+                if(!temp.empty()){
+                    for(string j : temp){
+                        if(j.empty())
+                            ans.push_back(aux);
+                        else
+                            ans.push_back(aux + " " + j);
+                    }
+                }
+            }
+        }
+        dp[in] = ans;
+        return ans;
+    }
+    vector<string> wordBreak(string s, vector<string>& dict) {
+        dp.clear();
+        unordered_set<string>se;
+        for(string i : dict){
+            se.insert(i);
+        }
+        vector<string> ans = solv(s,se,0);
+        return ans;
+    }
+};
