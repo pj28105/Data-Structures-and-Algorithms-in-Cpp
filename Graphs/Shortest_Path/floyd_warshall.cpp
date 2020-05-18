b3ff84b4457f82b71b699e0746943a9ca4dbb756
pj28105 <piyushjain964349@gmail.com>
2020-05-18 21:02:39 +0530

    Added floyd warshall

---
+++ Graphs/Shortest_Path/floyd_warshall.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int inf = INT_MAX;
+/*
+    Problem link->
+        https://practice.geeksforgeeks.org/problems/implementing-floyd-warshall/0
+    Article link->
+        https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/
+        
+    Tested on GFG
+*/
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int v;
+        cin >> v;
+        vector<vector<ll>> g(v,vector<ll>(v));
+        vector<vector<ll>> dist(v,vector<ll>(v,inf));
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < v; j++){
+                cin >> g[i][j];
+                dist[i][j] = g[i][j];
+            }
+        }
+        for(int k = 0; k < v; k++){
+            for(int i = 0; i < v; i++){
+                for(int j = 0; j < v; j++){
+                    dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < v; j++){
+                cout << dist[i][j] << " ";
+             }
+            cout << endl;
+        }   
+    }
+	return 0;
+}
