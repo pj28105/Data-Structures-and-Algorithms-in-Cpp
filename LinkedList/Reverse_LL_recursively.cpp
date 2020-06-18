d908e2f4cbc0fbe15cd6ee394026c9aff490e22b
pj28105 <piyushjain964349@gmail.com>
2020-06-18 21:31:53 +0530

    added reversing linked list recursively

---
+++ LinkedList/Reverse_LL_recursively.cpp
@@ -0,0 +1,75 @@
+//Initial Template for C++
+
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+/*
+    Problem link->
+        https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
+    
+    Tested on GFG
+*/
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+struct Node *reverseList(struct Node *head);
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+       head = reverseList(head);
+
+       printList(head);
+
+       cout << endl;
+    }
+    return 0;
+}
+
+
+Node* reverseList(Node *head){
+    if(!head or !head->next){
+        return head;
+    } 
+    Node* nxt = head->next;
+    Node* newHead= reverseList(head->next);
+    nxt->next = head;
+    head->next = NULL;
+    return newHead;
+}
+
