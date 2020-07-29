9c7f7f93b1f01642ed6c509554406f5c6ae6ac72
pj28105 <piyushjain964349@gmail.com>
2020-07-29 11:39:38 +0530

    modified minimum no of jumps

--- Dp/Minimum_no_of_jumps.cpp
+++ Dp/Minimum_no_of_jumps.cpp
@@ -1,43 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int solv(int a[],int n){
-    if(n == 1){
+/*
+    Problem link->
+        https://www.interviewbit.com/problems/min-jumps-array/
+    
+    Tested on Interview bit and leetocde!
+*/
+int jump(vector<int> &nums) {
+    if(nums.size() == 1){
         return 0;
     }
-    if(a[0] == 0){
-        return -1;
-    }
-    if(a[0] >= n - 1){
-        return 1;
-    }
-    int max_in = a[0],jump = 0,curr_max = a[0];
-    for(int i = 1; i < n; i++){
-        curr_max = max(curr_max,a[i] + i);
-        if(i == max_in){
-            jump++;
-            if(curr_max <= max_in){
-                return -1;
-            }
-            if(curr_max >= (n - 1)){
-                return jump + 1;
-            }
-            max_in = curr_max;
+    int st = 0;
+    int ans = 0,n = nums.size();
+    while(1){
+        if((st + nums[st]) >= n - 1){
+            return ans + 1;
         }
-    }
-    return -1;
-}
-
-int main(void){
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        int a[n];
-        for(int i = 0; i < n; i++){
-            cin >> a[i];
+        ans++;
+        int limit = st + nums[st],max_reach = 0,max_st = 0;
+        for(int i = st + 1; i <= limit; i++){
+            if(max_reach < (nums[i] + i)){
+                max_reach = nums[i] + i;
+                max_st = i;
+            }
         }
-        cout << solv(a,n) << endl;
-    }   
+        if(st >= max_st){
+            return -1;
+        }
+        st = max_st;
+    }
+    return ans;      
 }
