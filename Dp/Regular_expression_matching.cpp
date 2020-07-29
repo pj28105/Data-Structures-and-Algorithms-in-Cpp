55d2df3d6daf861239784c537aa1be9c231dea91
pj28105 <piyushjain964349@gmail.com>
2020-07-29 18:17:11 +0530

    simplified wildcard expression matching code

--- Dp/Regular_expression_matching.cpp
+++ Dp/Regular_expression_matching.cpp
@@ -9,29 +9,30 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<bool>> dp(p.length() + 1,vector<bool>(s.length() + 1));
-        dp[0][0] = 1;
-        for(int i = 1; i <= p.length(); i++){
-            for(int j = 0; j <= s.length(); j++){
-                if(p[i - 1] == '*'){
-                    dp[i][j] = dp[i - 2][j];
-                    if(j > 0 && (p[i - 2] == s[j - 1] || p[i - 2] == '.')){
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
-                        dp[i][j] = dp[i][j] || dp[i][j - 1];
+        int m = s.length(),n = p.length();
+        bool dp[n + 1][m + 1];
+        memset(dp,false,sizeof(dp));
+        dp[0][0] = 1; 
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= m; j++){
+                if(j == 0){
+                    if(p[i - 1] == '*'){
+                        dp[i][j] = dp[i - 2][j];
                     }
-                }else if(p[i - 1] == '.' && j > 0){
-                    dp[i][j] = dp[i - 1][j - 1];
-                }else if(j > 0 && s[j - 1] == p[i - 1]){
-                    dp[i][j] = dp[i - 1][j - 1];
+                }else{
+                    if(p[i - 1] != '.' && p[i - 1] != '*'){
+                        dp[i][j] = p[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : false;
+                    }else if(p[i - 1] == '.'){
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }else{
+                        dp[i][j] = dp[i - 2][j];
+                        if(p[i - 2] == '.' || p[i - 2] == s[j - 1]){
+                            dp[i][j] = dp[i][j] || dp[i][j - 1];
+                        }
+                    }
                 }
             }
         }
-        // for(int i = 0; i <= p.length(); i++){
-        //     for(int j = 0; j <= s.length(); j++){
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        return dp[p.length()][s.length()];
+        return dp[n][m];
     }
 };
--- Dp/Wild_Card_Pattern_Matching.cpp
+++ Dp/Wild_Card_Pattern_Matching.cpp
@@ -10,26 +10,21 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.length(),n = p.length();
+        int n = p.length(),m = s.length();
         bool dp[n + 1][m + 1];
         memset(dp,false,sizeof(dp));
-        dp[0][0] = 1; 
+        dp[0][0] = 1;
         for(int i = 1; i <= n; i++){
             for(int j = 0; j <= m; j++){
                 if(j == 0){
-                    if(p[i - 1] == '*'){
-                        dp[i][j] = dp[i - 2][j];
-                    }
+                    dp[i][j] = p[i - 1] == '*' ? dp[i - 1][j] : false;
                 }else{
-                    if(p[i - 1] != '.' && p[i - 1] != '*'){
-                        dp[i][j] = p[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : false;
-                    }else if(p[i - 1] == '.'){
+                    if(p[i - 1] == '?'){
                         dp[i][j] = dp[i - 1][j - 1];
-                    }else{
-                        dp[i][j] = dp[i - 2][j];
-                        if(p[i - 2] == '.' || p[i - 2] == s[j - 1]){
-                            dp[i][j] = dp[i][j] || dp[i][j - 1];
-                        }
+                    }else if(p[i - 1] == '*'){
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    }else if(p[i - 1] == s[j - 1]){
+                        dp[i][j] = dp[i - 1][j - 1];
                     }
                 }
             }
