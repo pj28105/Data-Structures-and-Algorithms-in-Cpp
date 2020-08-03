08a9e0d415b7a1f678f355494cc5df1a61a258db
pj28105 <piyushjain964349@gmail.com>
2020-08-03 23:10:55 +0530

    Added longest substring with at most k distinct characters

---
+++ Strings/Longest_substring_with_at_most_k_distinct_characters.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+    Problem link->
+        https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+    
+    Tested on leetcode!
+*/
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+		int freq[256];
+		memset(freq, 0, sizeof(freq));
+		int i = 0, j = 0, cnt = 0, ans = 0;
+		while (i < int(s.length()))
+		{
+			if (freq[s[i]] == 0)
+			{
+				cnt++;
+			}
+			freq[s[i]]++;
+			if (cnt > k)
+			{
+				while (cnt > k)
+				{
+					freq[s[j]]--;
+					if (freq[s[j]] == 0)
+					{
+						cnt--;
+					}
+					j++;
+				}
+			}
+			ans = max(ans, i - j + 1);
+			i++;
+		}
+		return ans;
+    }
+};
