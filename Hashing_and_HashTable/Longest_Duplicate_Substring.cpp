823dc14c0fe8439868512978fd4f6b64643fa7e4
pj28105 <piyushjain964349@gmail.com>
2020-09-04 16:48:55 +0530

    fixed bug for longest duplicate substring

--- Hashing_and_HashTable/Longest_Duplicate_Substring.cpp
+++ Hashing_and_HashTable/Longest_Duplicate_Substring.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-/*  
+/*
     Problem link->
         https://leetcode.com/problems/longest-duplicate-substring/
     
+
+    Note-> Different substrings can give same hash!
     Tested on Leetcode!
 */
 const int mod = 1e9 + 7; 
@@ -21,29 +23,37 @@ public:
         }
     }
     
-    string check(string s,int len){
+    int check(string s,int len){
         int n = s.length();
-        unordered_map<int,int>ma;
+        unordered_map<int,vector<int>>ma;
         int currhash = 0;
         for(int i = 0; i < len; i++){
             currhash = (currhash + ((pow_p[len - i - 1] * int(s[i])) % mod)) % mod; 
         }
-        ma[currhash] = 0;
+        ma[currhash].emplace_back(0);
         for(int i = len; i < n; i++){
             currhash = ((currhash - ((int(s[i - len]) * pow_p[len - 1]) % mod)) + mod) % mod;    
             currhash = (((currhash * pow_p[1]) % mod) + int(s[i])) % mod;
             auto it = ma.find(currhash);
             if(it != ma.end()){
-                string sub1 = s.substr(it->second,len);
-                string sub2 = s.substr(i - len + 1,len);
-                if(sub1 == sub2){
-                    return sub1;
+                for(int x : it->second){
+                    bool match = 1;
+                    int st = i - len + 1;
+                    for(int j = 0; j < len; j++){
+                        if(s[x + j] != s[st + j]){
+                            match = 0;
+                            break;
+                        }
+                    }
+                    if(match){
+                        return x;
+                    }
                 }
             }else{
-                ma[currhash] = i - len + 1;
+                ma[currhash].emplace_back(i - len + 1);
             }
         }
-        return "";
+        return -1;
     }
     
     string longestDupSubstring(string s) {
@@ -52,17 +62,19 @@ public:
             isComputed = 1;
         }
         int st = 1,e = s.length();
+        int stIndex = -1,maxLen = 0;
         string ans = "";
         while(st <= e){
-            int mid = (st + e) >> 1;
-            string aux = check(s,mid);
-            if(aux == ""){
+            int mid = (st + e)/2;
+            int currSt = check(s,mid);
+            if(currSt == -1){
                 e = mid - 1;
             }else{
-                ans = aux;
+                stIndex = currSt;
+                maxLen = mid;
                 st = mid + 1;
             }
         }
-        return ans;
+        return stIndex == -1 ? "" : s.substr(stIndex,maxLen);
     }
 };
