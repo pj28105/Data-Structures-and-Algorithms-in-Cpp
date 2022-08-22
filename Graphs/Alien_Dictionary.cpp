9cf33082c554cd8e992c9c0157672a5e98fd6956
pj28105 <piyush.jain@hotstar.com>
2022-08-22 14:10:48 +0530

    Updated solution for problem alien dictionary

--- Graphs/Alien_Dictionary.cpp
+++ Graphs/Alien_Dictionary.cpp
@@ -1,81 +1,64 @@
-#include <bits/stdc++.h>
-using namespace std;
+/*
+    Problem link -:
+        https://leetcode.com/problems/alien-dictionary/
+    
+    Tested on Leetcode
 
-string printOrder(string[], int, int);
-string order;
-bool f(string a, string b) {
-    int p1 = 0;
-    int p2 = 0;
-    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
-        p1 = order.find(a[i]);
-        p2 = order.find(b[i]);
-    }
-
-    if (p1 == p2 and a.size() != b.size()) return a.size() < b.size();
-
-    return p1 < p2;
-}
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
-        string s[n];
-        for (int i = 0; i < n; i++) cin >> s[i];
-
-        string ss = printOrder(s, n, k);
-        order = "";
-        for (int i = 0; i < ss.size(); i++) order += ss[i];
-
-        string temp[n];
-        std::copy(s, s + n, temp);
-        sort(temp, temp + n, f);
-
-        bool f = true;
-        for (int i = 0; i < n; i++)
-            if (s[i] != temp[i]) f = false;
-
-        cout << f;
-        cout << endl;
-    }
-    return 0;
-}
-
-void dfs(char curr,unordered_set<char>&vis,unordered_map<char,unordered_set<char>>&g,string& ans){
-    vis.insert(curr);
-    for(char c : g[curr]){
-        if(vis.find(c) == vis.end()){
-            dfs(c,vis,g,ans);
+    Notes-:
+        1) Do checkout its editorial for understanding multiple corner cases
+*/
+class Solution {
+public:
+    string alienOrder(vector<string>& words) {
+        unordered_map<int, unordered_set<int>> graph;
+        unordered_map<char, int> indegree;
+        
+        // Below pre-processing will solve many corner cases
+        for(string s : words){
+            for(char c : s){
+                indegree[c] = 0;
+            }
         }
-    }
-    ans = curr + ans; 
-}
-
-string printOrder(string dict[], int n, int k) {
-    unordered_map<char,unordered_set<char>>g;
-    for(int i = 0; i < n; i++){
-        bool found = 0;
-        for(int j = i + 1; j < n; j++){
-            int minlen = min(dict[i].length(),dict[j].length());
-            for(int kl = 0; kl < minlen; kl++){
-                if(dict[i][kl] != dict[j][kl]){
-                    found = 1;
-                    g[dict[i][kl]].insert(dict[j][kl]);
+        for(int i = 1; i < words.size(); i++){
+            string currWord = words[i];
+            string prevWord = words[i - 1];
+            
+            if(prevWord.length() > currWord.length() && 
+                        prevWord.substr(0, currWord.length()) == currWord){
+                return "";
+            }
+            int minLength = min(currWord.length(), prevWord.length());
+            for(int j = 0; j < minLength; j++){
+                if(currWord[j] != prevWord[j]){
+                    if(!graph[prevWord[j]].count(currWord[j])){
+                        graph[prevWord[j]].insert(currWord[j]);
+                        indegree[currWord[j]]++;
+                    }    
                     break;
                 }
             }
-            if(found)
-                break;
         }
-    }
-    string ans = "";
-    unordered_set<char>vis;
-    for(int i = 0; i < 26; i++){
-        if(vis.find('a' + i) == vis.end() && g.find(i + 'a') != g.end()){
-            dfs('a' + i,vis,g,ans);
+        queue<char> q;
+        for(auto &entry : indegree){
+            if(entry.second == 0){
+                q.push(entry.first);
+            }
         }
+        string res = "";
+        while(!q.empty()){
+            char currChar = q.front();
+            q.pop();
+            res += currChar;
+            for(char c : graph[currChar]){
+                indegree[c]--;
+                if(indegree[c] == 0){
+                    q.push(c);
+                }
+            }
+        }
+        if(res.size() < indegree.size()) {
+            return "";
+        }
+        return res;
     }
-    return ans;
-}
+};
